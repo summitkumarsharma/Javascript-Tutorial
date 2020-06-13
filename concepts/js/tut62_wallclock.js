790dd4ba5c84a0e036cd02b455210e495efe4f17
Sumit Sharma <summitkumarsharma@gmail.com>
2020-06-13 15:41:34 +0530

    new functionality on wall clock

--- concepts/js/tut62_wallclock.js
+++ concepts/js/tut62_wallclock.js
@@ -4,10 +4,12 @@ function updateClock() {
 
     // get the current time
     let currentTime = new Date();
+    // currentTime = currentTime.toLocaleDateString()
+    var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 
-    let month = currentTime.getMonth();
-    let day = currentTime.getDate();
-    let year = currentTime.getFullYear();
+    // let month = currentTime.getMonth();
+    // let day = currentTime.getDate();
+    // let year = currentTime.getFullYear();
 
     let currentHours = currentTime.getHours();
     let currentMinutes = currentTime.getMinutes();
@@ -19,14 +21,14 @@ function updateClock() {
     currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
 
     // convert railway clock to AM/PM Claock
-    currentHours = (currentHours > 12) ? currentHours - 12 : currentHours;
-    currentHours = (currentHours == 0) ? 12 : currentHours;
+    // currentHours = (currentHours > 12) ? currentHours - 12 : currentHours;
+    // currentHours = (currentHours == 0) ? 12 : currentHours;
 
     // choose AM/PM as per time of day
-    let timeofDay = (currentHours <= 12) ? "AM" : "PM"
+    let timeofDay = (currentHours < 12) ? "AM" : "PM"
 
     // prepare the timeString from hour, minute and seconds
-    let currentTimeString = currentTime + currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeofDay;
+    let currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeofDay + " on " + currentTime.toLocaleDateString('en-US', options);
 
     // insert the timestring in the DOM
     document.getElementById('clock').innerHTML = currentTimeString;
